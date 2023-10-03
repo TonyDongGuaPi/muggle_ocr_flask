710d5e4aa561911a5cc7b88d1a5e0bd5cd67ed6b
TonyDongGuaPi <49157052+TonyDongGuaPi@users.noreply.github.com>
2023-10-03 12:17:28 +0800

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,8 @@
 
 import base64
 import muggle_ocr
-
+from PIL import Image
+from io import BytesIO
 from flask import Flask, request
 
 sdk_Captcha = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.Captcha)
@@ -17,7 +18,21 @@ def index1(mode="captcha"):
         text = ''
         try:
             if mode == "captcha":
-                text = sdk_Captcha.predict(image_bytes=base64.b64decode(request.get_data().decode()))
+                image_data = base64.b64decode(request.get_data().decode())
+                image = Image.open(BytesIO(image_data))
+
+                # 创建一个白色底图像
+                background = Image.new('RGB', image.size, (255, 255, 255))
+
+                # 将图像粘贴到白色底图像上，忽略透明度
+                background.paste(image, (0, 0), mask=image.convert('RGBA'))
+
+                # 将图像转换为字节流
+                output_buffer = BytesIO()
+                background.save(output_buffer, format='PNG')
+                image_data = output_buffer.getvalue()
+
+                text = sdk_Captcha.predict(image_bytes=image_data)
             if mode == "ocr":
                 text = sdk_OCR.predict(image_bytes=base64.b64decode(request.get_data().decode()))
         except Exception as e:
